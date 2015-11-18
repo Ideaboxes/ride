b23ac588d42eb0dc76f04dea8abf50800fb832df
Maythee Anegboonlap <null@llun.in.th>
2015-11-18 19:48:20 +0800

    Add constructor and constants

--- map/pane.js
+++ map/pane.js
@@ -1,7 +1,28 @@
 'use strict'
 
+const CENTER = 0
+  , TOP = 1
+  , RIGHT = 2
+  , BOTTOM = 3
+  , LEFT = 4
+
 class Pane {
 
+  constructor(options) {
+    this.options = options || {
+      size: 3000,
+      degree: 0.1
+    }
+  }
+
+  key(location, position) {
+  }
 }
+
+Pane.CENTER = CENTER
+Pane.TOP = TOP
+Pane.RIGHT = RIGHT
+Pane.BOTTOM = BOTTOM
+Pane.LEFT = LEFT
 
 module.exports = Pane
