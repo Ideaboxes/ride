3d614f40879ae2d22b842a3cd3581f839799d910
Maythee Anegboonlap <null@llun.in.th>
2015-12-27 22:44:23 +0800

    Render logout menu

--- public/js/components/navigation.js
+++ public/js/components/navigation.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-export default () => (
-  <div className="top-bar navigation">
-    <div className="top-bar-left">
-      <ul className="menu">
-        <li className="menu-text">Flame</li>
-      </ul>
-    </div>
-    <div className="top-bar-right">
-      <ul className="menu">
-        <li className="menu-text"><Link to="/login">Login</Link></li>
-      </ul>
-    </div>
-  </div>
-  )
+class Navigation extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { loading: true, user: null }
+
+    fetch('/users/me', { credentials: 'include' })
+      .then(data => data.json())
+      .then(json => {
+        let user = null
+        if (json.user) {
+          user = json.user
+        }
+        this.setState({ loading: false, user: user })
+      })
+  }
+
+  render() {
+    let userMenu = null
+    if (!this.state.loading) {
+      userMenu = (<li className="menu-text"><Link to="/login">Login</Link></li>)
+      if (this.state.user) {
+        userMenu = <li className="menu-text"><a href="/users/logout">Logout</a></li>
+      }
+    }
+
+    console.log (this.state)
+
+    return (
+      <div className="top-bar navigation">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Flame</li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            {userMenu}
+          </ul>
+        </div>
+      </div>)
+  }
+
+}
+
+
+export default Navigation
