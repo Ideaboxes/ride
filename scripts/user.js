f315858ddcb4e56ec878fb17fc294fd5a8471848
Maythee Anegboonlap <null@llun.me>
2016-03-26 16:10:38 +0800

    Create activity record after fetch list of activities

--- scripts/user.js
+++ scripts/user.js
@@ -10,35 +10,42 @@ let Activity = require('../app/models/activity');
 let listUser = () => {
   User.findAll()
     .then(users => users[0].getServices())
-    .then(services => {
-      services.forEach(service => {
-        let date = new Date();
-        let beforeDate = moment(date).add(1, 'd').format('YYYY-MM-DD');
+    .then(services =>
+      new Promise((resolve, reject) => {
+        if (services.length === 0) return;
+        services.forEach(service => {
+          let date = new Date();
+          let beforeDate = moment(date).add(1, 'd').format('YYYY-MM-DD');
 
-        request.get({
-          url: 'https://api.fitbit.com/1/user/-/activities/list.json',
-          headers: {
-            Authorization: `Bearer ${service.accessToken}`,
-          },
-          qs: { beforeDate, offset: 0, limit: 20, sort: 'desc' },
-          json: true,
-        }, (error, result, body) => {
-          if (error) {
-            // Handle error here
-            console.error(error);
-            return;
-          }
-
-          let activities = body.activities;
-          let pagination = body.pagination;
-
-          console.log(pagination);
-          activities.forEach(activity => {
-
-            console.log(activity);
+          request.get({
+            url: 'https://api.fitbit.com/1/user/-/activities/list.json',
+            headers: {
+              Authorization: `Bearer ${service.accessToken}`,
+            },
+            qs: { beforeDate, offset: 0, limit: 20, sort: 'desc' },
+            json: true,
+          }, (error, result, body) => {
+            if (error) return reject(error);
+            return resolve(body);
           });
         });
-      });
+      }))
+    .then(body => {
+      let activities = body.activities;
+      let pagination = body.pagination;
+
+      console.log(pagination);
+      return Promise.all(activities.map(activity =>
+        Activity.create({
+          logID: activity.logId,
+          loaded: false,
+          distance: activity.distance,
+          duration: activity.duration,
+          startTime: activity.startTime,
+        })));
+    })
+    .then(activities => {
+      console.log(activities);
     });
 };
 
