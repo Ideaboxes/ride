050d6fc58560c535cf7e8ef9f9146a297bbbc287
Maythee Anegboonlap <null@llun.me>
2016-04-17 21:21:05 +0800

    Add fetching user activities action

--- public/js/actions.js
+++ public/js/actions.js
@@ -3,11 +3,18 @@
 
 export const SET_USER = 'SET_USER';
 export const UNLINK_USER_SERVICE = 'UNLINK_USER_SERVICE';
+export const SET_USER_ACTIVITIES = 'SET_USER_ACTIVITIES';
+
+export const FETCHING_USER_ACTIVITIES = 'FETCHING_USER_ACTIVITIES';
 
 /* Action creators
  */
 export function setUser(user) {
   return { type: SET_USER, user };
+}
+
+export function setUserActivities(activities) {
+  return { type: SET_USER_ACTIVITIES, activities };
 }
 
 export function unlinkService(name) {
@@ -18,6 +25,16 @@ export function unlinkService(name) {
       .then(data => data.json())
       .then(json => {
         dispatch(setUser(json.user));
+      });
+  };
+}
+
+export function fetchUserActivities() {
+  return dispatch => {
+    fetch('/v1/users/activities.json', { credentials: 'include' })
+      .then(data => data.json())
+      .then(json => {
+        dispatch(setUserActivities(json));
       });
   };
 }
--- public/js/reducers/application.js
+++ public/js/reducers/application.js
@@ -6,6 +6,10 @@ const initialState = {
 
 export default function Application(state = initialState, action) {
   switch (action.type) {
+    case Actions.SET_USER_ACTIVITIES:
+      return Object.assign({}, state, {
+        activities: action.activities,
+      });
     case Actions.SET_USER:
       return Object.assign({}, state, {
         user: action.user,
